e401d8d8887dfb3d9ed63eccd4023502952ca153
xiongjianqiang <xiongjianqiang@idwzx.com>
2019-02-25 11:13:05 +0800

    Fix multiprocessing logging

--- cacheer/settings.py
+++ cacheer/settings.py
@@ -8,13 +8,14 @@ def setup_logging(settings):
     """
 
     try:
-        # This is a fix for logging in multiple processes
-        # Source: https://github.com/jruere/multiprocessing-logging.git
-        from cacheer import multiprocessing_logging
-        multiprocessing_logging.install_mp_handler()
         logging.config.dictConfig(settings)
     except:
         logging.basicConfig(level=logging.INFO)
+    
+    # This is a fix for logging in multiple processes
+    # Source: https://github.com/jruere/multiprocessing-logging.git
+    from cacheer import multiprocessing_logging
+    multiprocessing_logging.install_mp_handler()
 
 
 def load_config(path):
